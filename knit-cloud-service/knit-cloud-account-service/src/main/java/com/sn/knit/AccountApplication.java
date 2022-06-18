75e8fb6f1c28dde5bce2ebb9337315936c7b0f19
ssh <278921135>
2022-06-18 09:47:13 +0800

    新增 前期准备

---
+++ knit-cloud-service/knit-cloud-account-service/src/main/java/com/sn/knit/AccountApplication.java
@@ -0,0 +1,24 @@
+package com.sn.knit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * <h1>用户账户微服务启动入口</h1>
+ * 127.0.0.1:8003/ecommerce-account-service/swagger-ui.html
+ * 127.0.0.1:8003/ecommerce-account-service/doc.html
+ *
+ * @author Administrator*/
+@EnableJpaAuditing
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AccountApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
