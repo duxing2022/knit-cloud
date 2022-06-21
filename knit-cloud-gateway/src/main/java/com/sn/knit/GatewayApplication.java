fccd47996ff4aa50e2fc8b5cc2fdcfcc6679e06d
ssh <278921135>
2022-06-22 00:24:52 +0800

    新增 Gateway网关

---
+++ knit-cloud-gateway/src/main/java/com/sn/knit/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.sn.knit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 网关启动入口
+ *
+ * @author Administrator
+ * @date 2022/06/22
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
